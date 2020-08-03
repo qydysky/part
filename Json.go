1db81782c12a04d26edbf7c8cb5bbe4ddddd510f
qydysky <qydysky@foxmail.com>
2020-08-03 14:43:29 +0800

    30

--- Json.go
+++ Json.go
@@ -17,3 +17,20 @@ func (*json) GetValFrom(file,key string)interface {}{
 	}
 	return jq.Find(key)
 }
+
+func (this *json) GetMultiValFrom(file string,key []string) []interface{}{
+	var jq *gojsonq.JSONQ
+	if Checkfile().IsExist(file) {
+		jq = gojsonq.New().File(file)
+	}else{
+		jq = gojsonq.New().FromString(file)
+	}
+
+	var returnVal []interface{}
+	for _,i := range key {
+		jq.Reset()
+		returnVal = append(returnVal,jq.Find(i))
+	}
+
+	return returnVal
+}
