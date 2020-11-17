b989fc77f39b62cf91b4909964e8e0a2c52bbf93
qydysky <qydysky@foxmail.com>
2020-11-18 07:19:44 +0800

    92

--- msgq/Msgq.go
+++ msgq/Msgq.go
@@ -7,15 +7,19 @@ type msgq struct {
 
 func New() (*msgq) {
 	l := new(msgq)
-	(*l).o = make(chan interface{})
-	(*l).men = make(chan bool)
+	(*l).o = make(chan interface{},1e9)
+	(*l).men = make(chan bool,1e9)
 	return l
 }
 
 func (m *msgq) Push(msg interface{}) {
-	if len(m.men) == 0  {return}
-	for <- m.men {
+	num := len(m.men)
+	for len(m.men) != 0 {
+		<- m.men
+	}
+	for num > 0 {
 		m.o <- msg
+		num -= 1
 	}
 	for len(m.o) != 0 {
 		<- m.o
