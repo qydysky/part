87bf17fa1a8e0a5ae93164b3d6789a63aa940b67
qydysky <qydysky@foxmail.com>
2023-05-14 23:39:27 +0800

    add

--- msgq/Msgq_test.go
+++ msgq/Msgq_test.go
@@ -2,7 +2,6 @@ package part
 
 import (
 	"context"
-	"fmt"
 	_ "net/http/pprof"
 	"sync"
 	"testing"
@@ -359,7 +358,7 @@ func Test_msgq3(t *testing.T) {
 
 	time.Sleep(time.Second)
 	for fin_turn := 0; fin_turn < 1000000; fin_turn += 1 {
-		fmt.Printf("\r%d", fin_turn)
+		// fmt.Printf("\r%d", fin_turn)
 		mq.Push_tag(`A1`, fin_turn)
 		if fin_turn != <-mun_c {
 			t.Fatal(fin_turn)
