bee11f02795f2bd88de73b0038ca90d9e3304ff7
qydysky <qydysky@foxmail.com>
2024-04-17 14:35:57 +0000

    1

---
+++ component2/comp.go
@@ -0,0 +1,43 @@
+package component2
+
+import (
+	"errors"
+	"runtime"
+	"strings"
+)
+
+var pkgInterfaceMap = make(map[string]any)
+
+var (
+	ErrEmptyPkgId = errors.New("ErrEmptyPkgId")
+	ErrRegistered = errors.New("ErrRegistered")
+)
+
+func PkgId() string {
+	if pc, _, _, ok := runtime.Caller(1); ok {
+		return strings.TrimSuffix(runtime.FuncForPC(pc).Name(), ".init")
+	}
+	return ""
+}
+
+func Register[TargetInterface any](pkgId string, _interface TargetInterface) error {
+	if pkgId == "" {
+		return ErrEmptyPkgId
+	}
+	if _interfaceReg, ok := pkgInterfaceMap[pkgId]; ok && _interfaceReg != nil {
+		return ErrRegistered
+	} else {
+		pkgInterfaceMap[pkgId] = _interface
+	}
+	return nil
+}
+
+func Get[TargetInterface any](pkgId string, defaultInterface ...TargetInterface) (_interface TargetInterface) {
+	if tmp, ok := pkgInterfaceMap[pkgId].(TargetInterface); ok {
+		return tmp
+	}
+	if len(defaultInterface) > 0 {
+		return defaultInterface[0]
+	}
+	return
+}
---
+++ component2/comp_test.go
@@ -0,0 +1,33 @@
+package component2
+
+import (
+	"testing"
+)
+
+type B struct{}
+
+func (b B) AddOne(a int) int {
+	return a + 1
+}
+
+func init() {
+	if e := Register[a]("github.com/qydysky/part/component2", B{}); e != nil {
+		panic(e)
+	}
+	aa = Get[a](pkgid)
+}
+
+type a interface {
+	AddOne(int) int
+}
+
+// or var aa = Get[a](pkgid)
+var aa a
+
+var pkgid = PkgId()
+
+func Test(t *testing.T) {
+	if aa.AddOne(1) != 2 {
+		t.Fatal()
+	}
+}
