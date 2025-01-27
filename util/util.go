4974221f5c3621d0062a6d892b7e0e24a81e5fda
qydysky <qydysky@foxmail.com>
2025-01-27 10:50:18 +0800

    1

--- util/util.go
+++ util/util.go
@@ -2,21 +2,28 @@ package part
 
 import (
 	"reflect"
+	"time"
 )
 
 //语法糖
 
-//数组切片，重新分配内存
+// 数组切片，重新分配内存
 // i := []int{0,1,2}
 // b := SliceCut(i[:1]).([]int)
 func SliceCopy(src interface{}) (des interface{}) {
 	srcV := reflect.ValueOf(src)
-	if sk := srcV.Kind();sk != reflect.Slice && sk != reflect.Array {
-		panic(&reflect.ValueError{Method:"reflect.Copy", Kind:sk})
+	if sk := srcV.Kind(); sk != reflect.Slice && sk != reflect.Array {
+		panic(&reflect.ValueError{Method: "reflect.Copy", Kind: sk})
 	}
-	desV := reflect.MakeSlice(srcV.Type(),srcV.Len(),srcV.Len())
+	desV := reflect.MakeSlice(srcV.Type(), srcV.Len(), srcV.Len())
 	reflect.Copy(desV, srcV)
 	des = desV.Interface()
 	return
 }
 
+func Callback(f func(startT time.Time, args ...any)) func(args ...any) {
+	now := time.Now()
+	return func(args ...any) {
+		f(now, args...)
+	}
+}
