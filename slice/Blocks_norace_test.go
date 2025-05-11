e022b9f39024506364116b51bb40eda6659577da
qydysky <qydysky@foxmail.com>
2025-05-12 02:49:05 +0800

    1

---
+++ slice/Blocks_norace_test.go
@@ -0,0 +1,30 @@
+//go:build !race
+
+package part
+
+import (
+	"runtime/debug"
+	"testing"
+)
+
+func TestMain3(t *testing.T) {
+	defer debug.SetGCPercent(debug.SetGCPercent(-1))
+	buf := NewFlexBlocks[byte]()
+	if tmpbuf, putBack, e := buf.Get(); e == nil {
+		tmpbuf = append(tmpbuf[:0], []byte("123")...)
+		// do something with tmpbuf
+		putBack(tmpbuf)
+	} else {
+		t.Fail()
+	}
+	if tmpbuf, putBack, e := buf.Get(); e == nil {
+		if cap(tmpbuf) != 8 {
+			t.Fatal()
+		}
+		tmpbuf = append(tmpbuf[:0], []byte("123")...)
+		// do something with tmpbuf
+		putBack(tmpbuf)
+	} else {
+		t.Fail()
+	}
+}
