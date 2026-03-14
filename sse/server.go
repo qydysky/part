d690db4e6e6852bea09b2f1e02718fb599e1cd74
qydysky <qydysky@foxmail.com>
2026-03-14 18:52:03 +0800

    SSE

---
+++ sse/server.go
@@ -0,0 +1,122 @@
+package server
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"unsafe"
+
+	pmq "github.com/qydysky/part/msgq"
+	pp "github.com/qydysky/part/pool"
+	pw "github.com/qydysky/part/web"
+)
+
+type Server struct {
+	mq       *pmq.MsgType[*Umsg]
+	connPool pp.PoolBlockI[Umsg]
+}
+
+func NewServer() (s *Server) {
+	s = new(Server)
+	s.mq = pmq.NewType[*Umsg]()
+	s.connPool = pp.NewPoolBlock(func() *Umsg {
+		return &Umsg{
+			Id:   uintptr(unsafe.Pointer(new(int))),
+			Key:  make([]byte, 8),
+			Data: make([]byte, 1024),
+		}
+	})
+	return
+}
+
+func (t *Server) MQ() *pmq.MsgType[*Umsg] {
+	return t.mq
+}
+
+// 用于httpSer.Handle里。接入和断开管道各收到一次消息
+//
+// 0. 发送到t.mq `init`
+//
+// 1. 尝试开始sse连接，如有错误，发送到t.mq `error`
+//
+// 2. 监听t.mq `send`，并发送
+//
+// 2. 监听t.mq `close`，控制连接断开
+//
+// 2. 将接收到的数据发送到t.mq `recv`
+//
+// 2. 如有错误，发送到t.mq `error`
+//
+// 3. 连接结束，发送到t.mq `fin`
+func (t *Server) Handle(w http.ResponseWriter, r *http.Request) {
+	umsg := t.connPool.Get()
+	defer t.connPool.Put(umsg)
+
+	t.mq.Push_tag(`init`, umsg)
+	defer t.mq.Push_tag(`fin`, umsg)
+
+	for cu := 0; true; {
+		umsg.Data = umsg.Data[0:cap(umsg.Data)]
+		if n, e := io.ReadFull(r.Body, umsg.Data[cu:]); e != nil {
+			cu += n
+			umsg.Data = umsg.Data[:cu]
+			break
+		} else {
+			cu += n
+			umsg.Data = append(umsg.Data, []byte{}...)
+		}
+	}
+
+	w = pw.WithFlush(w)
+	w.Header().Set(`Connection`, `keep-alive`)
+	w.Header().Set(`Transfer-Encoding`, `chunked`)
+	w.Header().Set(`Content-Type`, `text/event-stream`)
+
+	defer t.mq.Pull_tag_only(`send`, func(u *Umsg) (disable bool) {
+		if u.Id != 0 && u.Id != umsg.Id {
+			return false
+		}
+		if len(u.Key) != 0 {
+			if _, u.Err = w.Write(u.Key); u.Err != nil {
+			} else if _, u.Err = w.Write([]byte{':', ' '}); u.Err != nil {
+			} else if _, u.Err = w.Write(u.Data); u.Err != nil {
+			} else if _, u.Err = w.Write([]byte{'\n'}); u.Err != nil {
+			}
+		} else if _, u.Err = w.Write([]byte{'\n'}); u.Err != nil {
+		}
+		if u.Err != nil {
+			t.mq.Push_tag(`error`, u)
+			return true
+		} else {
+			return false
+		}
+	})()
+
+	cancle, ch := t.mq.Pull_tag_chan(`close`, 2, context.Background())
+	defer cancle()
+
+	t.mq.Push_tag(`recv`, umsg)
+	for {
+		if id := (<-ch).Id; id == 0 || id == umsg.Id {
+			break
+		}
+	}
+}
+
+type Umsg struct {
+	Id   uintptr
+	Err  error
+	Key  []byte
+	Data []byte
+}
+
+func (t *Umsg) Set(key, data []byte) *Umsg {
+	t.Key = key
+	t.Data = data
+	return t
+}
+
+func (t *Umsg) End() *Umsg {
+	t.Key = t.Key[:0]
+	return t
+}
